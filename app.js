02f08c7df89d57b8fe4ea53d81f5e6ae0d0d07c5
unknown <akhigbesadiq@gmail.com>
2023-03-29 16:53:53 +0100

    change the Procfile

--- app.js
+++ app.js
@@ -57,56 +57,9 @@ app.post("/failure", function(req, res){
   res.redirect("/")
 })
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("server is running on port 3000");
 });
-
-
-
-
-
-
-
-// //
-// mailchimp.setConfig({
-//   apiKey: "d6cc093094ab92971674e2a8f07d1dd2-us8",
-//   server: "us8",
-// });
-// const list_id = "bea9012d63";
-// const run = async () => {
-//   const data = await mailchimp.lists.batchListMembers(list_id, {
-//   members: [{
-//     email_address: email,
-//     status: "subscribed",
-//     merge_fields: {
-//       FNAME: firstName,
-//       LNAME: lastName
-//     }
-//   }]
-// };
-// const jsonData = JSON.stringify(data);
-//
-// const url = "https://us8.api.mailchimp.com/3.0/list/bea9012d63";
-// const options = {
-//   method: "POST",
-//   auth: "smooth1: d6cc093094ab92971674e2a8f07d1dd2-us8"
-// }
-//
-// const request = https.request(url, options, function(response) {
-//   response.on("data", function(data) {
-//     console.log(JSON.parse(data));
-//   })
-// })
-//
-//
-// request.write(jsonData);
-// request.end();
-//
-// });
-//
-
-
-
 
 
 // <!-- api key
